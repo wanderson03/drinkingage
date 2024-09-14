54d02428ff69162827eb88a89d806e0d19000733
wanderson03 <wanderson38@student.cccd.edu>
2024-09-14 02:03:43 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# drinkingage
+
+[Edit in StackBlitz next generation editor ⚡️](https://stackblitz.com/~/github.com/wanderson03/drinkingage)
---
+++ drinking-age.js
@@ -0,0 +1,20 @@
+// DO NOT CHANGE ANYTHING BETWEEN THE LINEs BELOW.
+
+const prompCreator = require('prompt-sync');
+
+const prompt = prompCreator();
+
+const age = prompt('Enter a number: ');
+
+console.log('age is ' + age);
+
+// TODO: Checks the given age to see whether it's legal to drink in the US or not.
+// Set to true if the age is 21 years or older. Otherwise, set to false.
+const result = undefined;
+
+// DO NOT CHANGE ANYTHING BELOW THIS LINE.
+console.log('result= ' + result);
+
+module.exports = {
+  result,
+};
